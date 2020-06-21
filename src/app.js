91308850c80f83ce8d8075e9e41c36ff9e4909fc
Sting Alleman <stingalleman@icloud.com>
2020-06-21 12:18:06 +0200

    cycle thru all months

--- src/app.js
+++ src/app.js
@@ -11,60 +11,27 @@ moment.locale("nl");
 const app = express();
 const calData = {};
 
-cron.schedule("0 5 * * *", async function () {
+async function init() {
 	try {
 		const browser = await puppeteer.launch({
-			headless: true,
+			headless: false,
 			args: ["--no-sandbox"],
 		});
 		const page = await browser.newPage();
-		await page.goto("https://www.alfrink.nl/agenda", {
-			waitUntil: "networkidle0",
-		});
-		let dateData = await page.evaluate(() => {
-			// eslint-disable-next-line no-undef
-			const calmonth = document.querySelector(".calendar__month");
-			return calmonth.textContent;
-		});
-		dateData = dateData.split(" - ");
-		const data = await page.$$eval("table tr td", (tds) =>
-			tds.map((td) => {
-				if (td.className == "is-disabled") {
-					return;
-				} else {
-					console.log(td);
-					return td.innerText;
-				}
-			})
-		);
-		let i;
-		for (i = 0; i < data.length; i++) {
-			if (data[i] == undefined) {
-				console.log("undefined");
-			} else if (/^[0-9]*$/.test(data[i]) == false) {
-				data[i] = data[i].slice(3);
-				data[i] = data[i].replace(/(\r\n|\n|\r)/gm, " +++ ");
-				calData[i] = {
-					// prettier-ignore
-					date: `${moment().year(dateData[1]).format("YYYY")}-${moment().month(dateData[0]).format("MM")}-${moment().date(i + 1).format("DD")}T10:10:10`,
-					info: data[i],
-				};
-				cal.createEvent({
-					// prettier-ignore
-					start: `${moment().year(dateData[1]).format("YYYY")}-${moment().month(dateData[0]).format("MM")}-${moment().date(i + 1).format("DD")}T10:10:10`,
-					summary: data[i],
-					location: "Alfrink College",
-					allDay: true,
-				});
-				console.log("push!");
-			}
+		let a;
+		for (a = 1; a <= 12; a++) {
+			await page.goto(`https://www.alfrink.nl/agenda?month=${a}&year=${moment().year()}`, {
+				waitUntil: "networkidle0",
+			});
 		}
 		await browser.close();
 		return calData;
 	} catch (err) {
 		console.log("error: " + err);
 	}
-});
+}
+
+init();
 
 app.get("/", function (req, res) {
 	res.send("/alfrink of /alfrink/data");
