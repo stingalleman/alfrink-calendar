0263a9b0325bf15d080407ccdddd1c2ade523a0c
Sting Alleman <stingalleman@icloud.com>
2020-06-24 19:58:20 +0200

    guess no sandbox is back

--- src/app.js
+++ src/app.js
@@ -106,6 +106,7 @@ async function main() {
 		});
 		const browser = await puppeteer.launch({
 			headless: true,
+			args: ["--no-sandbox"],
 		});
 		const page = await browser.newPage();
 		let c;
